1a800ef52b993bfc8b85fdfd0eda4535986bc274
Michal <michalpalkowski3@wp.pl>
2025-04-17 11:42:19 +0200

    addr fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cargo run --release -- \
-  --contract-address 0x038785c9c4f93101a6f1ca968ed253240718b5f77f969cf505d293e96ff1d249 \
-  --proxy-address 0x06f1491009c80bc09ff7e14efbd0ad94d1842fe4887b4d89287b9d5781d78e1b \
+  --contract-address 0x02249d24dfbe04e84e796f2d0de7f280196720bb21fa040a6b34b9dac92b919b \
+  --proxy-address 0x07d1a17d7881adb769f7870bd670da293532a34e88f8c512bd9e59c60a5e2345 \
   --start-block 1 \
   --rpc-url "http://localhost:5050" \
   --event-name "GameFinished" \
   --atlantic-key "" \
-  --account-address  \
-  --account-private-key 
+  --account-address 0x1f401c745d3dba9b9da11921d1fb006c96f571e9039a0ece3f3b0dc14f04c3d \
+  --account-private-key 0x7230b49615d175307d580c33d6fda61fc7b9aec91df0f5c1a5ebe3b8cbfee02
